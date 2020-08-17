4ee8d0520a58f786026bcb0c943039b2218cacdb
Brandon Little <brandonlittle1999@hotmail.com>
2020-08-17 10:51:57 +0100

    Added logo and created the layout of the widgets.

--- lottoGen.py
+++ lottoGen.py
@@ -3,27 +3,36 @@ import tkinter as tk
 # This creates the window object for the application
 window = tk.Tk()
 
-window.title('Lotto Generator')
+img = tk.PhotoImage(file = 'logo.gif')
 
+window.title('Lotto Generator')
+window.resizable(0, 0)
+
+# Creating the widgets
+imageLabel = tk.Label(window, image = img)
 label1 = tk.Label(window, text = '...', relief='groove', width = 2)
 label2 = tk.Label(window, text = '...', relief='groove', width = 2)
 label3 = tk.Label(window, text = '...', relief='groove', width = 2)
 label4 = tk.Label(window, text = '...', relief='groove', width = 2)
 label5 = tk.Label(window, text = '...', relief='groove', width = 2)
 label6 = tk.Label(window, text = '...', relief='groove', width = 2)
-getNumbers = tk.Button(window, text = 'Get your lotto numbers', relief='groove')
-resetNumbers = tk.Button(window, text = 'Reset the numbers', relief='groove')
+getNumbers = tk.Button(window, text = 'Get Your Lotto Numbers')
+resetNumbers = tk.Button(window, text = 'Reset Numbers')
 
-label1.grid()
-label2.grid()
-label3.grid()
-label4.grid()
-label5.grid()
-label6.grid()
-getNumbers.grid()
-resetNumbers.grid()
+# Designing the layout of the widgets
+imageLabel.grid(row = 1, column = 1, rowspan = 2)
+label1.grid(row = 1, column = 2, padx = 10)
+label2.grid(row = 1, column = 3, padx = 10)
+label3.grid(row = 1, column = 4, padx = 10)
+label4.grid(row = 1, column = 5, padx = 10)
+label5.grid(row = 1, column = 6, padx = 10)
+label6.grid(row = 1, column = 7, padx = (10, 20))
+getNumbers.grid(row = 2, column = 2, columnspan = 4)
+resetNumbers.grid(row = 2, column = 6, columnspan = 2)
 
-window.geometry("500x500")
+
+
+
 
 # The mainloop syntax sustains the window being open for the application
 window.mainloop()
